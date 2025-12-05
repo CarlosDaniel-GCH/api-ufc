0bb566486fed0d2a1a90b3bb99b3caf48f7ad550
CarlosDaniel-090304 <carlosdanielchilcon19@gmail.com>
2025-12-05 14:34:05 -0500

    Agregando componente verMas

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import VerMas from "./components/verMas";
 
 function Inicio(){
     return(
         <div>
             <Header />
+            <VerMas />
             <Footer />
         </div>
     )
